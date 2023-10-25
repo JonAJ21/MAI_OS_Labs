925862a8dcaec98de1434532ddeb3001d6335e8c
JonAJ <jonajmail@gmail.com>
2023-10-25 16:21:30 +0300

    Some fixes

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -43,7 +43,7 @@ int main() {
     std::srand(std::time(nullptr));
     std::vector<Point> points = make_random_sample(POINTS_COUNT, MAX_COORD);
 
-    int k = 2; // Число кластеров
+    int k = 4; // Число кластеров
     int steps = 30; // Количество итераций, за которое мы будем искать кластеры
     std::vector<Claster> clasters(k); // Наши кластеры
     // Сначала случайным образом выставляем на этой же плоскости k центров кластеров
@@ -62,21 +62,13 @@ int main() {
         out << k << std::endl;
     }
     
-
-
-
-
-
     // Запускаем цикл поиска кластеров
     for (int step = 0; step < steps; ++step) {
-        std::cout << "Clasters centers: " << std::endl;
-        for (int i = 0; i < k; ++i) {
-            std::cout << clasters[i].center.x << ' ' << clasters[i].center.y << ' ' << i << std::endl;
-        }
         // Очищаем точки принадлежащие кластеру
         for (int i = 0; i < k; ++i) {
             clasters[i].points.clear();
         }
+
         // Сначала мы «привязываем» каждую точку к тому центроиду, к которому она ближе
         for (int j = 0; j < POINTS_COUNT; ++j) {
             int closest_claster_id = 0;
@@ -90,6 +82,7 @@ int main() {
             points[j].claster = closest_claster_id;
             clasters[closest_claster_id].points.push_back(points[j]);
         }
+
         // Передаем начальные кластеры в файл
         if (step == 0) {
             for (int i = 0; i < k; ++i) {
@@ -108,6 +101,17 @@ int main() {
                 new_claster_center.x += point.x;
                 new_claster_center.y += point.y;
             }
+            // Проверка выделили мы все кластеры (Если нет, то рандомим все центры еще раз)
+            if (clasters[i].points.size() == 0) {
+                std::cout << "ZEERROORORO" << std::endl;
+                for (int j = 0; j < k; ++j) {
+                    new_claster_center.x = std::rand() % MAX_COORD;
+                    new_claster_center.y = std::rand() % MAX_COORD;
+                    clasters[j].center = new_claster_center;
+                }
+                break;
+            }
+            
             new_claster_center.x /= clasters[i].points.size();
             new_claster_center.y /= clasters[i].points.size();
             clasters[i].center = new_claster_center;
@@ -125,17 +129,6 @@ int main() {
         }
     }
  
-    
-    // // for debug
-    // for (auto claster : clasters) {
-    //     std::cout << "Claster №: " << claster.center.claster << std::endl;
-    //     for (auto point : claster.points) {
-    //         std::cout << point.x << ' ' << point.y << std::endl;
-    //     }
-    // }
-
-
-
 
     out.close();
     return 0;
